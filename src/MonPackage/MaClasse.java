f3f3d99ef0c9115e8cc923cc4de21d3f18156c80
33645 <33645@10.21.136.232>
2023-11-06 19:11:35 +0700

    premier test

--- src/MonPackage/MaClasse.java
+++ src/MonPackage/MaClasse.java
@@ -3,7 +3,7 @@ package MonPackage;
 public class MaClasse {
 	 public static void main(String[] args) 
 	 {
-	  System.out.println("Obj2: NbreObjVoiture ");
+	  System.out.println("Obj2: NbreObjVoiturell ");
 	  }
 
 }
