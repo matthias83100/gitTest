4c09143c723c312e449d60890cc85918d4fbbd7a
33645 <33645@10.21.136.232>
2023-11-06 20:07:45 +0700

    DEUXIEME

--- src/MonPackage/MaClasse.java
+++ src/MonPackage/MaClasse.java
@@ -3,7 +3,7 @@ package MonPackage;
 public class MaClasse {
 	 public static void main(String[] args) 
 	 {
-	  System.out.println("Obj2: NbreObjVoiturell ");
+	  System.out.println("Obj2: NbreObjVoiturel2l ");
 	  }
 
 }
